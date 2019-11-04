172fa8a89c9caef9bf99fa887df5583f5fdc6de9
Callum Stringer <callum.stringer@idorsia.com>
2019-11-04 15:42:12 +0100

    first commit

---
+++ js/justSomeJS.js
@@ -0,0 +1,20 @@
+document,addEventListener('DOMContentLoaded', function(){
+var elems = document.querySelectorAll('.parallax');
+var instances  = M.Parallax.init(elems);
+});
+
+
+document,addEventListener('DOMContentLoaded', function(){
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+    
+    console.log("ey this is working");
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
